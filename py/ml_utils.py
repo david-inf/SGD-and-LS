Fixed objective function bug, now remove gradient computation in order to run the algorithms for the epochs number

py/ml_utils.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb  1 18:33:32 2024
-
-@author: Utente
-"""
 
 import numpy as np
 import pandas as pd
@@ -30,6 +24,7 @@ def optim_data(models):
     models_data = pd.DataFrame(
         {
             "Solver": [model.solver for model in models],
+            "C": [model.C for model in models],
             "Minibatch": [model.minibatch for model in models],
             "Step-size": [model.opt_result.step_size for model in models],
             "Momentum": [model.opt_result.momentum for model in models],
@@ -45,6 +40,28 @@ def optim_data(models):
         }
     )
     return models_data
+
+
+def optim_bench(model):
+    data = pd.DataFrame(
+        {
+            "Solver": model.solver,
+            "C": model.C,
+            "Minibatch": np.nan,
+            "Step-size": np.nan,
+            "Momentum": np.nan,
+            "Solution": [model.coef_],
+            "Loss": model.loss,
+            "Grad norm": model.grad,
+            "Run-time": np.nan,
+            "Iterations": model.opt_result.nit,
+            # "Termination": model.message,
+            "Train accuracy": model.accuracy_train,
+            "Test accuracy": model.accuracy_test,
+            "Loss/Epochs": np.nan
+        }
+    )
+    return data
 
 
 def plot_loss(models, labels, title=None, start=0, end=200):
@@ -132,7 +149,7 @@ def test_plots(data):
     bar_width = 0.35
     rects1 = axs[1].bar(data["labels"], data["Run-time"], bar_width)
     axs[1].grid(True)
-    axs[1].set_ylabel("Run-time")
+    axs[1].set_ylabel("Run-time (seconds)")
     axs[1].bar_label(rects1, fmt="%.4f", padding=3)
     axs[1].set_xticks(x, data["labels"], rotation=90)
     # 3) Accuracy

py/models.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 import numpy as np
 from sklearn.metrics import accuracy_score
@@ -34,6 +33,9 @@ class LogisticRegression():
             self.coef_ = model.x
             self.loss = model.fun
             self.grad = np.linalg.norm(model.jac)
+            self.epochs = None
+            self.tol = None
+            self.minibatch = None
         elif self.solver in ["SGD-Fixed", "SGD-Decreasing", "SGD-Armijo"]:
             model = solver_dict[self.solver](w0, X_train, y_train, self.C,
                     self.minibatch, step_size, self.epochs, self.tol)

py/solvers_utils.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb  1 09:27:23 2024
-
-@author: Utente
-"""
 
 import numpy as np
 
@@ -39,9 +33,9 @@ def logistic(w, X, y, lam=1):
     -------
     scalar
     """
-    N = X.shape[0]
-    log_arg = 1 + np.exp(- y * np.dot(X, w))
-    L = np.sum(np.log(log_arg)) / N  # loss
+    # N = X.shape[0]
+    L_vec = np.log(1 + np.exp(- y * np.dot(X, w)))
+    L = np.sum(L_vec)  # loss
     O = 0.5 * np.linalg.norm(w) ** 2  # regularizer
     return L + lam * O
 
@@ -59,28 +53,28 @@ def logistic_der(w, X, y, lam=1, coeff=1):
     -------
     vector like w.shape[0]
     """
-    N = X.shape[0]
+    # N = X.shape[0]
     r = - y * sigmoid(- y * np.dot(X, w))
-    dL = np.dot(r, X) / N
+    dL = np.dot(r, X)
     dO = w
     return dL + coeff * lam * dO
 
 
 def f_and_dfnorm(w, X, y, lam=1):
-    N = X.shape[0]
+    # N = X.shape[0]
     z = - y * np.dot(X, w)  # compute one time instead on two
-    return (np.sum(np.log(1 + np.exp(z))) / N + lam * np.linalg.norm(w) ** 2,  # objective
-            np.linalg.norm(np.dot(- y * sigmoid(z), X) / N + lam * w))  # gradient norm
+    return (np.sum(np.log(1 + np.exp(z))) + lam * np.linalg.norm(w) ** 2,  # objective
+            np.linalg.norm(np.dot(- y * sigmoid(z), X) + lam * w))  # gradient norm
 
 
 def f_and_df(w, X, y, lam=1):
     # fun, jac = f_and_df()
     # fun, _ = f_and_df()
     # _, jac = f_and_df()
-    N = X.shape[0]
+    # N = X.shape[0]
     z = - y * np.dot(X, w)  # compute one time instead on two
-    return (np.sum(np.log(1 + np.exp(z))) / N + lam * 0.5 * np.linalg.norm(w) ** 2,  # objective
-            np.dot(- y * sigmoid(z), X) / N + lam * w)  # gradient
+    return (np.sum(np.log(1 + np.exp(z))) + lam * 0.5 * np.linalg.norm(w) ** 2,  # objective
+            np.dot(- y * sigmoid(z), X) + lam * w)  # gradient
 
 
 # useless